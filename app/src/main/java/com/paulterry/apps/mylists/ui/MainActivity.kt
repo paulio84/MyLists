13013b833bf8afaf07ece7672b7f82c8b8ad6b88
Paul Terry <paul.terry84@gmail.com>
2021-03-14 09:56:27 +0000

    added navigation between ListFragment and ItemFragment

--- app/src/main/java/com/paulterry/apps/mylists/ui/MainActivity.kt
+++ app/src/main/java/com/paulterry/apps/mylists/ui/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.paulterry.apps.mylists.ui
 
 import android.os.Bundle
-import androidx.activity.viewModels
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.paulterry.apps.mylists.adapters.MyListsAdapter
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.paulterry.apps.mylists.R
 import com.paulterry.apps.mylists.databinding.ActivityMainBinding
-import com.paulterry.apps.mylists.viewModels.MyListsViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _ ->
+            binding.addItemFAB.visibility = if (nd.id == nc.graph.startDestination) View.VISIBLE else View.GONE
+        }
+
+        binding.addItemFAB.setOnClickListener { navController.navigate(R.id.action_listFragment_to_itemFragment)}
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
