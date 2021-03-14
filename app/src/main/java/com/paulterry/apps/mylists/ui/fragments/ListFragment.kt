b83ba38d8878fdda93721e2fbc19f61048073c75
Paul Terry <paul.terry84@gmail.com>
2021-03-14 10:05:28 +0000

    added shared view model and displayed list items in ListFragment

--- app/src/main/java/com/paulterry/apps/mylists/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/paulterry/apps/mylists/ui/fragments/ListFragment.kt
@@ -5,14 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.paulterry.apps.mylists.R
+import androidx.fragment.app.activityViewModels
+import com.paulterry.apps.mylists.adapters.MyListsAdapter
+import com.paulterry.apps.mylists.databinding.FragmentListBinding
+import com.paulterry.apps.mylists.viewModels.MyListsViewModel
 
 class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private val sharedViewModel: MyListsViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.listItemRecyclerView.adapter = MyListsAdapter(sharedViewModel.items)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
